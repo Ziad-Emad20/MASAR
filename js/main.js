20d5f73763b59a7a2d02ea82162ed6d9102b8ca5
Ziad Emad <ziaemad0111@gmail.com>
2024-09-08 01:15:51 +0300

    Finish Header

---
+++ js/main.js
@@ -0,0 +1,29 @@
+//! This function Change background Navigation and fixed Navigation 
+function changeNav() {
+    var navbar = document.querySelector('#navbar');
+    var navbarResponsive = document.querySelector('.contact-nav-small');
+    var scrollValue = window.scrollY;
+    if (scrollValue < 10) {
+        navbar.classList.remove('fixed-top')
+        navbarResponsive.classList.remove('fixed-top')
+        navbar.style.cssText = "background-color: var(--backGroundNavMiddel)"
+        navbarResponsive.style.cssText = "background-color: var(--backGroundNavMiddel)"
+    }
+    else {
+        navbar.classList.add('fixed-top')
+        navbarResponsive.classList.add('fixed-top')
+        navbar.style.cssText = "background-color: var(--backGroundNavTop)"
+        navbarResponsive.style.cssText = "background-color: var(--backGroundNavTop)"
+    }
+}
+window.addEventListener('scroll', changeNav)
+// ! Side nav
+
+let openSide = document.querySelector('#openSide');
+let closeSide = document.querySelector('#closeSide');
+openSide.addEventListener('click', function () {
+    sideNav.style.cssText = "transform: translateX(0)";
+});
+closeSide.addEventListener('click', function () {
+    sideNav.style.cssText = "transform: translateX(-100%)";
+});
