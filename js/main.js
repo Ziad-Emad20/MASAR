628b10625f441766d2c52a0e9952995ef2f12035
Ziad Emad <ziaemad0111@gmail.com>
2024-09-09 02:38:35 +0300

    Create servise section

--- js/main.js
+++ js/main.js
@@ -1,24 +1,23 @@
 //! This function Change background Navigation and fixed Navigation 
 function changeNav() {
     var navbar = document.querySelector('#navbar');
-    var navbarResponsive = document.querySelector('.contact-nav-small');
+    var navbarResponsive = document.querySelector('#navbarSmall');
     var scrollValue = window.scrollY;
     if (scrollValue < 10) {
         navbar.classList.remove('fixed-top')
         navbarResponsive.classList.remove('fixed-top')
         navbar.style.cssText = "background-color: var(--backGroundNavMiddel)"
-        navbarResponsive.style.cssText = "background-color: var(--backGroundNavMiddel)"
+        // navbarResponsive.style.cssText = "background-color: var(--backGroundNavMiddel)"
     }
     else {
         navbar.classList.add('fixed-top')
         navbarResponsive.classList.add('fixed-top')
         navbar.style.cssText = "background-color: var(--backGroundNavTop)"
-        navbarResponsive.style.cssText = "background-color: var(--backGroundNavTop)"
+        // navbarResponsive.style.cssText = "background-color: var(--backGroundNavTop)"
     }
 }
 window.addEventListener('scroll', changeNav)
 // ! Side nav
-
 let openSide = document.querySelector('#openSide');
 let closeSide = document.querySelector('#closeSide');
 openSide.addEventListener('click', function () {
@@ -28,14 +27,14 @@ closeSide.addEventListener('click', function () {
     sideNav.style.cssText = "transform: translateX(-100%)";
 });
 
-// navTaps
-// Initialization for ES Users
-import { Tab, initMDB } from "mdb-ui-kit";
+// // navTaps
+// // Initialization for ES Users
+// import { Tab, initMDB } from "mdb-ui-kit";
 
-initMDB({ Tab });
+// initMDB({ Tab });
 
-//form
-// Initialization for ES Users
-import { Input, initMDB } from "mdb-ui-kit";
+// //form
+// // Initialization for ES Users
+// import { Input, initMDB } from "mdb-ui-kit";
 
-initMDB({ Input });
+// initMDB({ Input });
